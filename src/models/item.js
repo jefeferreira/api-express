157340e9b70e81ccc24f22baa0ec229a7a2aac16
Jeferson Ferreira <jefeferreira23@hotmail.com>
2024-10-05 16:21:55 -0300

    creating item model

---
+++ src/models/item.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model('Item', itemSchema);
+
+export default Item;
